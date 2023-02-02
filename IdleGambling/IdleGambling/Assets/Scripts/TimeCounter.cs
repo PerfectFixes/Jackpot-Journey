d92c788884981b790aa1e2cf713e08519db71a6b
PerfectFixes <ttay29@gmail.com>
2023-02-02 18:49:57 +0200

    Finished AFK mode totally
    [+] Coin animation to let the player know when he gets a coin.
    [+] VFX of smoke to help disappearing the coin animation
    [+] Architecture change for main loop, moved all delays to after the recursive command

--- IdleGambling/IdleGambling/Assets/Scripts/TimeCounter.cs
+++ IdleGambling/IdleGambling/Assets/Scripts/TimeCounter.cs
@@ -26,11 +26,31 @@ public class TimeCounter : MonoBehaviour
     [Tooltip("The text of the timer (that counts the elapsed time")]
     [SerializeField] private TMP_Text timerText;
 
+    [Header("Material")]
+    [Tooltip("The material who controls the animation")]
+    [SerializeField] private Material cointMaterial;
+
+    private float progress;
+    private bool isReady;
+    private bool resetAnimation;
+
+    [Header("GameObjects")]
+    [SerializeField] private GameObject smokeAnimation;
+
+    private void Awake()
+    {
+        //disable the smoke animation
+        smokeAnimation.SetActive(false);
+    }
     private void Start()
     {
-
+        resetAnimation = false;
         //Sets the amount of reward the player will get to 0
         score = 0;
+
+        isReady = false;
+        cointMaterial.SetFloat("_Progress", 0);
+
         PlayerPrefs.SetInt("AFK Reward", 0);
 
         //Change the next reward to the curret one + 30 seconds
@@ -41,7 +61,16 @@ public class TimeCounter : MonoBehaviour
     }
     private void Update()
     {
-      
+        //Make the animation and make sure it will stop
+        if (isReady)
+        {
+            float timeToComplete = 30;
+            progress += Time.deltaTime / timeToComplete;
+
+            cointMaterial.SetFloat("_Progress", progress);
+
+            progress %= 1f;
+        }
     }
     //When the button is pressed the scene will change back to the start
     public void LeaveScene()
@@ -51,6 +80,7 @@ public class TimeCounter : MonoBehaviour
 
     IEnumerator UpdateTime()
     {
+        
         //Get the amount of time in the scene
         timeInScene = Time.timeSinceLevelLoad;
 
@@ -65,16 +95,27 @@ public class TimeCounter : MonoBehaviour
 
         //Wait 1 second to display the numbers on screen
         yield return new WaitForSeconds(1);
-        
+
         //formatting the string into the correct format 
         string timeString = string.Format("{0:00}:{1:00}:{2:00}", hours, minutes, seconds);
 
         //Updating the UI
         timerText.text = timeString;
 
+       
+        //Repeat the function
+        StartCoroutine(UpdateTime());
+
         //Add coins
         if (timeInScene == targetSecondsForReward)
         {
+            //Restart the animation
+            resetAnimation = true;
+            smokeAnimation.SetActive(true);
+
+            //Stops the animation from restarting until the next cycle
+            isReady = false;
+
             score++;
 
             //Adds 30 seconds so the next reward will be after 30 seconds
@@ -82,9 +123,20 @@ public class TimeCounter : MonoBehaviour
 
             //Update the reward in a playerprefs
             PlayerPrefs.SetInt("AFK Reward", score);
-        }
 
-        //Repeat the function
-        StartCoroutine(UpdateTime());        
+            yield return new WaitForSeconds(0.2f);
+            //Reseting the progess
+            progress = 0;
+            cointMaterial.SetFloat("_Progress", progress);
+            yield return new WaitForSeconds(0.2f);
+        }
+        isReady = true;
+
+        if (resetAnimation)
+        {
+            resetAnimation = false;
+            yield return new WaitForSeconds(0.5f);
+            smokeAnimation.SetActive(false);
+        }
     }
 }
