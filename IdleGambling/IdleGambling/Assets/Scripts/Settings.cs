51ae83cece325a7ffbafbb5afc376a0bcfe3d285
PerfectFixes <ttay29@gmail.com>
2023-02-15 18:35:43 +0200

    UI Update and Fixes
    [+] When the player isn't in the needed level the AFK Mode and Auto Gamble is Hidden.
    [+] UI changed a bit to look better and symmetric.

--- IdleGambling/IdleGambling/Assets/Scripts/Settings.cs
+++ IdleGambling/IdleGambling/Assets/Scripts/Settings.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -8,9 +9,22 @@ using UnityEngine.SceneManagement;
 public class Settings : MonoBehaviour
 {
 
-    [Header("Settings Related")]
+    [Header("GameObjects")]
 
     [SerializeField] private GameObject settingsGameObject;
+
+
+    [Header("Buttons")]
+
+    [Tooltip("The button to activate sleep mode")]
+    [SerializeField] private Button sleepModeButton;
+
+
+
+    [Header("Togglers")]
+
+    [Tooltip("The toggle button of the auto gambling mode in the settings")]
+    [SerializeField] private Toggle autoGambleToggle;
 
     [Tooltip("The toggle button of enabling/disabling Music in the settings")]
     [SerializeField] private Toggle musicToggle;
@@ -18,13 +32,22 @@ public class Settings : MonoBehaviour
     [Tooltip("The toggle button of enabling/disabling SFX in the settings")]
     [SerializeField] private Toggle sfxToggle;
 
-    [Tooltip("The button to activate sleep mode")]
-    [SerializeField] private Button sleepModeButton;
+    [Header("Text")]
 
-    [Tooltip("The toggle button of the auto gambling mode in the settings")]
-    [SerializeField] private Toggle autoGambleToggle;
+    [Tooltip("The text of AFK Mode")]
+    [SerializeField] private TMP_Text afkModeOutsideText;
 
-    
+    [Tooltip("The text of AFK Mode inside of the button")]
+    [SerializeField] private TMP_Text afkModeButtonText;
+
+    [Tooltip("The text under the AFK Mode")]
+    [SerializeField] private TMP_Text afkModeUnlockLevelText;
+
+    [Tooltip("The text of Auto Gamble")]
+    [SerializeField] private TMP_Text autoGambleOutsideText;
+
+    [Tooltip("The text under the Auto Gamble")]
+    [SerializeField] private TMP_Text autoGambleUnlockLevelText;
 
     private void Awake()
     {
@@ -34,20 +57,38 @@ public class Settings : MonoBehaviour
 
         if (prestigeLevel >= 2)
         {
+            //Enables the button to go to the afk scene
             sleepModeButton.interactable = true;
+
+            //Remove the requierment of what needed to make this unlocked
+            afkModeUnlockLevelText.enabled = false;     
         }
         else
         {
+            //Disable the option of pressing the button until you reach Prestige level 2
             sleepModeButton.interactable = false;
+
+            //Hooks the player to make him want to know what is hiding beind level 2
+            afkModeOutsideText.text = "???? ????";
+
+            afkModeButtonText.text = "????";
         }
+
         if (prestigeLevel >= 5)
         {
-
+            //Enables the button to start auto gambling
             autoGambleToggle.interactable = true;
+
+            //Remove the requierment of what needed to make this unlocked
+            autoGambleUnlockLevelText.enabled = false;
         }
         else
         {
+            //Disable the option of pressing the button until you reach Prestige level 5
             autoGambleToggle.interactable = false;
+
+            //Hooks the player to make him want to know what is hiding beind level 5
+            autoGambleOutsideText.text = "???? ????";
         }
     }
 
